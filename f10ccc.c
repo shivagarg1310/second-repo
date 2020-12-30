30d9a86dd1edf361ced76707aa5a6d798bce5166
shivagarg1310 <shivagarg1310@gmail.com>
2020-12-30 18:00:32 +0530

    my second file

---
+++ f10ccc.c
@@ -0,0 +1,10 @@
+/* code for presendence and assoctivity */
+
+#include<stdio.h>
+
+void main()
+{
+	int a=13 , b=45, c= 354 , result;
+	result= a+b*c-c*a;
+   printf("desired result=%d" , result);
+}
